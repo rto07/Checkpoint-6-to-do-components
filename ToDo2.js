a866c0f7f95ed65b0a21240d4a1407da8f48c1a6
tar <rtorres845@gmail.com>
2018-10-09 14:20:51 -0500

    latest attempts

---
+++ ToDo2.js
@@ -0,0 +1,15 @@
+ import React, { Component } from 'react';
+
+ class ToDo extends Component {
+   render() {
+     return (
+    	<li>
+        <input type = "button" value = "Delete" onChange = { this.props.deleteToDo }/>
+       	<input type = "checkbox" checked = { this.props.isCompleted } onChange = { this.props.toggleComplete }/>
+       	<span> { this.props.description } </span>
+     	</li>
+     );
+   }
+ }
+
+ export default ToDo;
