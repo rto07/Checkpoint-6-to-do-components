5e1632316ad87408afdd62c551df3dce9cdf6828
tar <rtorres845@gmail.com>
2018-10-10 11:09:49 -0500

    Updated 11:10 AM work files are App.js and ToDo.js

--- ToDo.js
+++ ToDo.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 
-class ToDo extends React.Component {
+class ToDo extends Component {
 	render(){
 		return (
-			/*<li>{this.props.description }</li>*/
 			<li>
 				<input type = "checkbox" checked = {this.props.completed} onChange = { this.props.toggleComplete } />
 				<span> {this.props.description} </span>
 
-				< button onClick = { this.props.deleteTodo }> Delete </button>
+				< input type = "button" onClick = { this.props.deleteTodo} value = "Delete" onChange = {this.props.deleteTodo}/>
+
 			</li>
 			);
 	}
