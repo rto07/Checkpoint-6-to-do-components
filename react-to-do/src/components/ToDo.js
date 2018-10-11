2ff601498b8b4cb6c75f994412e16141de9ef121
tar <rtorres845@gmail.com>
2018-10-11 09:51:22 -0500

    to-do-Events Checkpoint Work

---
+++ react-to-do/src/components/ToDo.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class ToDo extends Component {
+	render(){
+		return (
+			<li>
+				<input type = "checkbox" checked = {this.props.completed} onChange = { this.props.toggleComplete } />
+				<span> {this.props.description} </span>
+
+				< input type = "button" onClick = { this.props.deleteTodo} value = "Delete"/>
+
+			</li>
+			);
+	}
+}
+
+export default ToDo;
